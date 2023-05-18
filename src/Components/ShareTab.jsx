f46dfd1dea671a4362f7518eab9e08b6f18a07c8
Amr Elsehety <amr.abdelazeem22@gmail.com>
2023-05-18 17:35:19 +0300

    Create Share tab

---
+++ src/Components/ShareTab.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function ShareTap(){
+    return <div className="share-tab">
+        <p>SHARE</p>
+        <img src="images/icon-facebook.svg" alt="facebook"/>
+        <img src="images/icon-twitter.svg" alt="twitter"/>
+        <img src="images/icon-pinterest.svg" alt="pinterest"/>
+    </div>
+}
+
+export default ShareTap;
